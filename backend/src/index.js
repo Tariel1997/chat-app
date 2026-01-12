e65e3b2a74f7b6435b213f45b6acab7430b0bf9b
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2026-01-12 22:01:26 +0400

    feat: add profile page and profile picture upload functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,8 @@ const app = express()
 
 const PORT = process.env.PORT
 
-app.use(express.json())
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(cookieParser())
 app.use(cors({
     origin: "http://localhost:5173",
