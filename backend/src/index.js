bad8f74cfd3b3aeed9a91d1e4ec033b31afd8a14
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2026-01-20 23:36:43 +0400

    chore: build frontend and prepare production assets

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,9 +9,12 @@ import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 import { app, server } from './lib/socket.js'
 
+import path from 'path'
+
 dotenv.config()
 
 const PORT = process.env.PORT
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
@@ -23,6 +26,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
     console.log("Server is running on port:" + PORT)
